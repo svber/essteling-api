4dcb8e4c7216101d80994d7cacaa8e9a41824e50
sven <srjberni@avans.nl>
2018-06-14 20:33:02 +0200

    g

--- server.js
+++ server.js
@@ -1,43 +1,21 @@
-var http = require('http');
 var express = require('express');
-var routes_v1 = require('./api/routes_v1');
-var routes_v2 = require('./api/routes_v2');
-
 var app = express();
-
 app.get('/', function(request, response) {
  response.send('Hello Avans!');
 })
-
-app.get('/json', function(request, response) {
- response.json({
- 'some_name': 'Value',
- 'an_array_of_objects': [
- {
- 'another_name': 'Another value',
- 'a_further_name': "A further value"
- },
- {'yet_another_name': 'Yet another value'}
- ],
- 'some_boolean': true,
- 'some_integer': 42,
- 'array_of_ints': [
- 2, 3, 5, 7, 11, 13
- ],
- 'array_of_strings': [
- "twee", "drie", "vijf", "zeven"
- ]
- })
+app.get('/about', function(request, response) {
+ response.send('Written by <jouw naam hier invullen>');
 })
-
+app.post('/', function(request, response) {
+ response.send('Hello Avans, POST request received!');
+})
+app.put('/', function(request, response) {
+ response.send('Hello Avans, PUT request received!');
+})
 app.get('*', function(request, response) {
  response.status(404);
  response.send('404 - Not found');
 })
-
-
-app.listen(process.env.PORT || 3000, function(){
-	console.log('De server luistert op port 3000');	
-});
-
-module.exports = app;
+app.listen(3000, function() {
+ console.log('Server app is listening on port 3000');
+})
