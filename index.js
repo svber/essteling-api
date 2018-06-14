7c2a8b0be6d114191ecdf8ae5eef4c20ffeb9714
sven <srjberni@avans.nl>
2018-06-14 20:10:36 +0200

    []

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(request, response) {
+	response.send('Hello Avans!');
+})
+
 app.get('/macAddrress', function(request, response) {
- response.send('Hello Avans!');
+	response.json({
+		'macAddrresses': [
+			{
+				'location': 'exit',
+				'macAddrress': "A further value",
+				'minor': '00'
+			},
+			{
+				'location': 'exit',
+				'macAddrress': "A further value",
+				'minor': '01'
+			},
+		],
+	})
 })
 
 app.all('*', function(request, response) {
- response.status(404);
- response.send('404 - Not found');
+	response.status(404);
+	response.send('404 - Not found');
 })
 
-app.listen(3000, function() {
- console.log('Server app is listening on port 3000');
-})
+app.listen(process.env.PORT || 3000, function(){
+	console.log('De server luistert op port 3000');	
+});
